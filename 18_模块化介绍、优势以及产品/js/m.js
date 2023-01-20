97ed06e8e79ce6ddd5e87bd1c0bc1c4eff02a93a
yjy1 <318277446@qq.com>
2023-01-20 13:01:32 +0800

    tag_浏览器使用ES6模块化引入模块

---
+++ 18_模块化介绍、优势以及产品/js/m.js
@@ -0,0 +1,7 @@
+/* 
+    Uncaught SyntaxError: The requested module './js/m.js' does not provide an export named 'default'
+*/
+export let school = '中心小学'
+export  function teach (){
+    console.log('教学');
+}
